fdd085f04e98475fa6da86381a587c2e54d6b350
DonSutherland95 <58916345+DonSutherland95@users.noreply.github.com>
2020-09-14 19:55:25 -0400

    initial commit

--- index.js
+++ index.js
@@ -2,44 +2,45 @@
 
 /**
  * ### Challenge `processFirstItem`
- * 
+ *
  * @instructions
  * Implement a higher-order function called `processFirstItem`.
  * It takes two arguments:
  * @param stringList an array of strings.
  * @param callback function that takes a string as its argument.
  * @returns the result of invoking `callback` with the FIRST element in `stringList`.
- * 
+ *
  * Example of usage of this higher-order function:
  * Invoking `processFirstItem` passing `['foo', 'bar']` and `(str) => str + str`,
  * should return 'foofoo'.
-*/
+ */
 function processFirstItem(stringList, callback) {
-  return callback(stringList[0])
+  return callback(stringList[0]);
 }
 
 // ⭐️ Example Challenge END ⭐️
-
 
 ///// M V P ///////
 
 /* Task 1: `counterMaker`
  * Study the code for counter1 and counter2. Answer the questions below.
- * 
- * 1. What is the difference between counter1 and counter2?
- * 
- * 2. Which of the two uses a closure? How can you tell?
- * 
- * 3. In what scenario would the counter1 code be preferable? In what scenario would counter2 be better? 
  *
-*/
+ * 1. What is the difference between counter1 and counter2? Counter1 is a higher order function that declares a counter in its block scope and returns a child function that increments that count variable by 1. Counter2 initilizes a variable in the global scope and then creates a function to increament the global variable count by 1.
+ *
+ * 2. Which of the two uses a closure? How can you tell?Counter1 uses a closure because it has access to the parent scope. The counter function which is the inner function has access to the outer function which is counterMaker.
+ *
+ * 3. In what scenario would the counter1 code be preferable? In what scenario would counter2 be better? Counter1 code would be preferable if you want to store data in a separate scope, and share it only where necessary. It allows the child function to easily access the parent function. Counter2 would only be preferable if you want multiple functions to have access to the global variable.
+ *
+ *
+ *
+ */
 
 // counter1 code
 function counterMaker() {
   let count = 0;
   return function counter() {
     count++;
-  }
+  };
 }
 
 const counter1 = counterMaker();
@@ -51,20 +52,19 @@ function counter2() {
   return count++;
 }
 
-
 /* Task 2: inning() 
 
 Write a function called `inning` that generates a random number of points that a team scored in an inning. This should be a whole number between 0 and 2. */
 
-function inning(/*Code Here*/){
-
-    /*Code Here*/
-
+function inning() {
+  let num = Math.floor(Math.random() * 3);
+  return num;
 }
+console.log(inning());
 
 /* Task 3: finalScore()
 
-Write a higher order function called `finalScore` that accepts the callback function `inning` (from above) and a number of innings and and returns the final score of the game in the form of an object.
+Write a higher order function called `finalScore` that accepts the callback function `inning` (from above) and a number of innings and returns the final score of the game in the form of an object.
 
 For example, 
 
@@ -74,20 +74,31 @@ finalScore(inning, 9) might return:
   "Away": 5,
 }
 
-*/ 
+*/
 
-function finalScore(/*code Here*/){
+function finalScore(getScore, numOfInning) {
+  let homeScore = 0;
+  let awayScore = 0;
+  for (let i = 0; i < numOfInning; i++) {
+    homeScore = homeScore + getScore();
+    awayScore = awayScore + getScore();
+    // console.log(homeScore, awayScore);
+  }
 
-  /*Code Here*/
-
+  let scoreBrd = {
+    Home: homeScore,
+    AwayScore: awayScore,
+  };
+  return scoreBrd;
 }
+console.log(finalScore(inning, 9));
 
 /* Task 4: 
 
 Create a function called `scoreboard` that accepts the following parameters: 
 
-(1) Callback function `getInningScore`
-(2) Callback function `inning`
+(1) Callback function `getInningScore` aka finalscore
+(2) Callback function `inning` aka inning()
 (2) A number of innings
 
 and returns the score at each pont in the game, like so:
@@ -104,8 +115,19 @@ and returns the score at each pont in the game, like so:
 
 Final Score: awayTeam - homeTeam */
 
-function scoreboard(/* CODE HERE */) {
-  /* CODE HERE */
+function scoreboard(getInningScore, inning, numOfInning) {
+  let homeScore = 0;
+  let awayScore = 0;
+  for (let i = 1; i <= numOfInning; i++) {
+    homeScore += inning();
+    awayScore += inning();
+    // console.log([i] + " inning: " + awayScore + " - " + homeScore);
+    getInningScore(inning, numOfInning);
+    // console.log([i], homeScore, awayScore);
+    console.log([i] + " inning " + homeScore + " - " + awayScore);
+  }
+  return "Final Score " + homeScore + " - " + awayScore;
+  // console.log("Final Score " + homeScore + " - " + awayScore);
 }
 
-
+console.log(scoreboard(finalScore, inning, 9));
